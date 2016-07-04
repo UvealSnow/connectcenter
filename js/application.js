15be13a6a7bd230ce87a6b76554a086303b8ae3f
UvealSnow <kevhah92@gmail.com>
2016-07-04 10:06:43 -0500

    Fixed JS

--- js/application.js
+++ js/application.js
@@ -1,31 +1,27 @@
-function hideSlide( slide ) {
+var activeSlide = '';
+
+function hideSlide (slide) {
 	TweenMax.to(slide, 2, { top:0, x:"100%", opacity:1});	
 }
 
-function closeButton( slide ) {
+function closeButton (slide) {
 	var cB = slide.find('.close-button');
 	cB.click(
 			function () {
-				hideSlide( slide );
+				hideSlide(slide);
+				activeSlide = '';
 			}
 		);
 }
-function showSlide( button ) {
+function showSlide (button) {
 	var slide = $("[data-slide=" + button.attr("data-button") + "]");
+	activeSlide = $("[data-slide=" + button.attr("data-button") + "]");
 	TweenMax.fromTo(slide, 1, { top:0, opacity: 1}, { top:0, x:"-100%", opacity:1});	
 
 	closeButton( slide );
 }
 $(document).ready(
 	function () {
-		/* 	
-
-		Set hr bars [data-separators] to 100% height
-	 	This needs to be done through javascript because 
-	 	since absolute positioned elements are out of the 
-	 	document's flow, they can not take the height of its parent.
-
-	 	*/
 	 	$("[data-separator]").css("height", $(window).innerHeight());
 	 	$("[data-button]").click( 
 	 		function () {
@@ -34,20 +30,26 @@ $(document).ready(
 
 	 	$('#scroll-0').click(function () {
 	 		$('#quienes').ScrollTo({ duration: 800, easing: 'linear' });
+	 		if (activeSlide != '') hideSlide(activeSlide);
 	 	});
 	 	$('#scroll-1').click(function () {
 	 		$('#inicio').ScrollTo({ duration: 800, easing: 'linear' });
+	 		if (activeSlide != '') hideSlide(activeSlide);
 	 	});
 	 	$('#scroll-2').click(function () {
 	 		$('#quienes').ScrollTo({ duration: 800, easing: 'linear' });
+	 		if (activeSlide != '') hideSlide(activeSlide);
 	 	});
 	 	$('#scroll-3').click(function () {
 	 		$('#servicios').ScrollTo({ duration: 800, easing: 'linear' });
+	 		if (activeSlide != '') hideSlide(activeSlide);
 	 	});
 	 	$('#scroll-4').click(function () {
 	 		$('#contacto').ScrollTo({ duration: 800, easing: 'linear' });
+	 		if (activeSlide != '') hideSlide(activeSlide);
 	 	});
 	 	$('#scroll-5').click(function () {
 	 		$('#trabajos').ScrollTo({ duration: 800, easing: 'linear' });
+	 		if (activeSlide != '') hideSlide(activeSlide);
 	 	});	
 	}); 
